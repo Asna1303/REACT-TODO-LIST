51260fd6be3978de1ac6ce81a9949359600ca2aa
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-11 09:38:48 +0530

    added correspponding components in each files

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const todo = () => {
+  return (
+    <div>
+      todo
+    </div>
+  )
+}
+
+
---
+++ src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Todoform = () => {
+  return (
+    <div>
+      Todoform
+    </div>
+  )
+}
+
+
