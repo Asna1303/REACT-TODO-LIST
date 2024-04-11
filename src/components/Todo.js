c0f500f84225dea88d9a64b823e4490de5f5001a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-11 21:09:04 +0530

    Imported fontawesomeicon,fapentosquare,fatrash

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
 
 export const todo = () => {
   return (
