4e4b67ed2458d552874cd7808f19e00cc3383d98
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-11 09:51:38 +0530

    added an input tag in the form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Todoform = () => {
   return (
-    <form>
-      Todoform
+    <form className="TodoForm">
+       <input type="text" className="todo-input" placeholder='What is the task today?' />
     </form>
   )
 }
