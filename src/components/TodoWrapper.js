61a6e0209248d415e7bfc6d1002232b773292281
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-11 20:54:39 +0530

    Implemented addTodo function to update todos state with a new todo item.

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -5,6 +5,12 @@ import { useState } from 'react';
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  }
   return (
     <div className="TodoWrapper">
  <TodoForm addTodo={addTodo} />
