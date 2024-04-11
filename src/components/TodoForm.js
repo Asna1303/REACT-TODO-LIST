0f4294da7ddd102ac0956b7669943dedca7e1936
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-11 09:52:32 +0530

    added a button

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,6 +4,7 @@ export const Todoform = () => {
   return (
     <form className="TodoForm">
        <input type="text" className="todo-input" placeholder='What is the task today?' />
+       <button type="submit" className='todo-btn'>Add Task</button>
     </form>
   )
 }
