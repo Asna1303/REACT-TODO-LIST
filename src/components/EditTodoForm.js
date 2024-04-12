6e22e971926171dbe51bda39ea35ffe23ac3538a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-12 11:26:55 +0530

    initialized value state

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 export const EditTodoForm = ({editTodo, task}) => {
+  const [value, setValue] = useState(task.task);
   return (
     <div>
       EditTodoForm
