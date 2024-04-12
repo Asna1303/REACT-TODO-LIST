a2e73c0866b0df9f1e85bac3adadc4f2d8f935b3
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-12 11:27:50 +0530

    Implement handleSubmit function for todo editing

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { useState } from 'react'
 export const EditTodoForm = ({editTodo, task}) => {
   const [value, setValue] = useState(task.task);
+  const handleSubmit = (e) => {
+    
+      e.preventDefault();
+     
+      editTodo(value, task.id);
+    };
   return (
     <div>
       EditTodoForm
