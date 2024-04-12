f3e94fe85c737ca5c6f32d063bbb6cb215706e2a
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-04-12 11:28:49 +0530

    Implement TodoForm component for editing tasks

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -9,9 +9,10 @@ export const EditTodoForm = ({editTodo, task}) => {
       editTodo(value, task.id);
     };
   return (
-    <div>
-      EditTodoForm
-    </div>
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+  </form>
   )
 }
 
